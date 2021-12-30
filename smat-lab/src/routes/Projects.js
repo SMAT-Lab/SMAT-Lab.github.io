961a2e3775c25b6de626d903926dd7727aa0366a
Li Li <lilicoding@gmail.com>
2021-12-30 11:22:04 +1100

    update

--- smat-lab/src/routes/Projects.js
+++ smat-lab/src/routes/Projects.js
@@ -12,21 +12,40 @@ export default function Projects(props) {
     return (
 
         <PageWrapper showDivider={true} title="Projects of Our Group">
-            <SubPageWrapper title="Funded Research Projects (Ongoing)">    
-                    <MyLi >
-                        safsfsfsdsafsfsf
-                        d <MyLink href="https://www.google.com/" >dsfdddds=+d</MyLink> d
-                        sdsafsfsfsdsafsfsfsdsaf asfsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsd
-                    </MyLi>
 
+            <SubPageWrapper title="Software Projects">    
+                    <MyLi> 
+                    <Grid xs={12} sm={6} item container style={{ padding: "", }}>
+                        <Grid xs={12} item container>
+                            <Typography align="justify" style={{ fontWeight: "18px", fontWeight: "700", }}>Scalpel: The Python Static Analysis Framework</Typography>
+                        </Grid>
+                        <Grid xs={12} item container>
+                            <Typography align="justify" style={{ fontWeight: "18px" }}>
+                                Scalpel is a Python Static Analysis Framework. 
+                                It provides essential program analysis functions for facilitating the implementation of client applications focusing on statically resolving dedicated problems in Python code.
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12} item container>
+                            <Typography align="justify" style={{ fontWeight: "18px" }}>
+                                Website: https://github.com/SMAT-Lab/Scalpel
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    </MyLi>
             </SubPageWrapper>
-            <SubPageWrapper title="Funded Research Projects (Completed)">                
+
+
+            <SubPageWrapper title="Funded Research Projects (Ongoing)">    
+                    
+            </SubPageWrapper>
+            <SubPageWrapper title="Funded Research Projects (Completed)">  
+            <!--              
                     <MyLi >
                         safsfsfsdsafsfsf
                         d <MyLink href="https://www.google.com/" >dsfdddds=+d</MyLink> d
                         sdsafsfsfsdsafsfsfsdsaf asfsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsdsafsfsfsd
                     </MyLi>
-           
+           -->
             </SubPageWrapper>
         </PageWrapper>
     );
