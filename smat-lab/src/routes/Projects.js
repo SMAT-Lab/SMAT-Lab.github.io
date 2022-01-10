713cd381e6627162de089de0a6c902688b4808fb
Li Li <lilicoding@gmail.com>
2022-01-11 09:24:13 +1100

    update

--- smat-lab/src/routes/Projects.js
+++ smat-lab/src/routes/Projects.js
@@ -13,7 +13,7 @@ export default function Projects(props) {
 
     return (
 
-        <PageWrapper showDivider={true} title="Software Projects">
+        <PageWrapper showDivider={true} title="Active Software Projects">
 
             <SubPageWrapper 
                 title="Scalpel: The Python Static Analysis Framework" 
@@ -24,9 +24,12 @@ export default function Projects(props) {
             >                
                 Scalpel is a Python Static Analysis Framework.
                 It provides essential program analysis functions for facilitating the implementation of client applications focusing on statically resolving dedicated problems in Python code.
+
+                <br /><br /><br />
+                <b>URL:</b> <MyLink href="https://github.com/SMAT-Lab/Scalpel">https://github.com/SMAT-Lab/Scalpel</MyLink>
             </SubPageWrapper>
 
-            <b>URL:</b> <MyLink href="https://github.com/SMAT-Lab/Scalpel">https://github.com/SMAT-Lab/Scalpel</MyLink>
+            
 
 
             <SubPageWrapper 
@@ -38,9 +41,12 @@ export default function Projects(props) {
                 AppScanner is an online apk scanning service developed by a group of Monash students.
                 AppScanner aims at providing a reliable online scanning service for pinpointing compatibility issues in Android apps. AppScanner endeavors to take into account all the possible types of compatibility issues,
                 including not only framework evolution-induced ones but also the device-specific ones brought by the tremendous fragmentation problem faced by the Android ecosystem.
+
+                <br /><br /><br />
+                <b>URL:</b> <MyLink href="http://appscanner.com.au/">http://appscanner.com.au/</MyLink>
             </SubPageWrapper>
 
-            <b>URL:</b> <MyLink href="http://appscanner.com.au/">http://appscanner.com.au/</MyLink>
+            
         </PageWrapper>
 
     );
