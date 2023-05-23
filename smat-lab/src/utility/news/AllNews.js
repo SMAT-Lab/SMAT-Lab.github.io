f3a61d9dd54f1dfc4bbdc1afcc17ee165ac99558
DannyGoo <lyhcoming@gmail.com>
2023-05-23 16:00:20 +1000

    update faculty

--- smat-lab/src/utility/news/AllNews.js
+++ smat-lab/src/utility/news/AllNews.js
@@ -1,25 +1,31 @@
 const AllNews = {
+    "2023": [
+        {
+            "date": "June. 2023",
+            "content": "Xiaoyu Sun became Assistant Professor at Australian National University (ANU)."
+        }
+    ],
     "2022": [
         {
-            "date": "Apr. 8, 2022",
+            "date": "Apr. 2022",
             "content": "One paper accepted to ISSTA 2022. Big congratulations to our PhD students Pei Liu and Yanjie Zhao."
         },
         {
-            "date": "Feb. 24, 2022",
+            "date": "Feb. 2022",
             "content": "One paper accepted to ACL 2022, one of the best venues in Natural Language Processing. Big congratulations to our visiting student Juncai Guo."
         },
         {
-            "date": "Jan. 17, 2022",
+            "date": "Jan. 2022",
             "content": "One paper accepted to IEEE Transactions on Software Engineering (TSE), one of the best journals in Software Engineering. Big congratulations to Yanjie Zhao.",
         },
         {
-            "date": "Jan. 14, 2022",
+            "date": "Jan. 2022",
             "content": "Two papers accepted to TheWebConf 2022. Big congratulations to Zhensu and Guosheng.",
         }
     ],
     "2021": [
         {
-            "date": "Dec. 06, 2021",
+            "date": "Dec. 2021",
             "content": "Four papers accepted to ICSE 2022 (ICSE is the best conference in Software Engineering).",
         }
     ]
